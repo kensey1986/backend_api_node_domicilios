7c225206543fbd8fe5be252a838d73d30599b9bb
Desarrollo <onlin1992@gmail.com>
2021-05-11 13:39:33 -0500

    agregadas variables de mongo

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-  MONGODB_URI: process.env.DB_CNN_STRING || "mongodb://localhost/apicompany",
+  MONGODB_URI: process.env.DB_CNN_STRING || "mongodb+srv://admin:kenshin1986@cluster0.hr2hs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   PORT: process.env.PORT || 4000,
   SECRET: 'products-api',
 };
